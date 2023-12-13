3ba6745c6202cea465659ec326ea7474484f9521
Guyacamole <guy@cs3.mx>
2023-12-13 14:20:28 -0600

    url

--- webapp/src/api/api.js
+++ webapp/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://montoya.rgrox.com:5000';
+const BASE_URL = 'http://api-gateway:5000';
 
 export const api = axios.create({ baseURL: BASE_URL });
 
